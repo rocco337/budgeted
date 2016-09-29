9f46283f4a2883f429baeb3aa10a8b4f7c7c27aa
rocco337 <roko.bobic@gmail.com>
2016-09-29 22:10:48 +0200

    added default css layout

--- src/Budgeted.Frontend/gulpfile.js
+++ src/Budgeted.Frontend/gulpfile.js
@@ -1,8 +1,22 @@
 var gulp = require('gulp');
 var bower = require('gulp-bower');
 var mainBowerFiles = require('main-bower-files');
+var uglify = require('gulp-uglify');
+var cleanCSS = require('gulp-clean-css');
+var concatCss = require('gulp-concat-css');
 
-gulp.task('default',function(){
-    return gulp.src(mainBowerFiles()).pipe(gulp.dest('build'));
-})
+
+gulp.task('default', function (d) {    
+    return gulp
+        .src([
+            "bower_components/pure/base.css",
+            "bower_components/pure/grids.css",
+            "bower_components/pure/buttons.css",
+            "bower_components/pure/menus.css",
+            "bower_components/pure/forms.css",
+            "assets/*.css"])       
+        .pipe(concatCss("bundle.css")) 
+        .pipe(cleanCSS())
+        .pipe(gulp.dest('out/'));
+});
 
