413edc7c91ab204ddf2b67db92fb5709601c4cc0
rocco337 <roko.bobic@gmail.com>
2017-01-06 14:29:13 +0100

    create new transaction api added

--- src/Budgeted.Api/wwwroot/Api/transactionsController.cs
+++ src/Budgeted.Api/wwwroot/Api/transactionsController.cs
@@ -1,23 +1,50 @@
+using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Mvc;
+using System;
 
-namespace Api{
+namespace Api
+{
 
     [Route("api/[controller]")]
-    public class TransactionsController:Controller{
+    public class TransactionsController : Controller
+    {
 
-            [Route("{searchPhrase}")]
-            [HttpGet()]
-            public IActionResult Get(string searchPhrase){
-                return Ok(new []{
+        [Route("{searchPhrase}")]
+        [HttpGet()]
+        public IActionResult Get(string searchPhrase)
+        {
+            return Ok(new[]{
                     new TransactionDto(){ Id=1, Description="Test", DateTimeFormated = "1.1.2017 12:00", Tags=new []{"hous","car","insurance"} },
                 });
-            }
+        }
+
+        [Route("")]
+        [HttpPost()]
+        public IActionResult Add([FromBody] TransactionAddRequest request)
+        {
+            return Ok(request);
+        }
     }
 
-    public class TransactionDto{
+    public class TransactionDto
+    {
         public int Id { get; set; }
         public string Description { get; set; }
         public string DateTimeFormated { get; set; }
+        public string[] Tags { get; set; }
+    }
+
+    public class TransactionAddRequest
+    {
+        [Required]
+        public decimal Amount { get; set; }
+
+        [Required]        
+        public string Description { get; set; }
+        
+        [Required]        
+        public string TransactionDate { get; set; }
+
         public string[] Tags { get; set; }
     }
 }
