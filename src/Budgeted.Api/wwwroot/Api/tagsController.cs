870852911a35bfa68c4fe3a81a59efb3badb8a7c
rocco337 <roko.bobic@gmail.com>
2017-01-06 13:25:10 +0100

    add tags controller

---
+++ src/Budgeted.Api/wwwroot/Api/tagsController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api{
+    
+    [Route("api/[controller]")]
+    public class TagsController : Controller{
+
+        [Route("")]
+        [HttpGet()]
+
+        public IActionResult GetAllTags(){
+            return Ok(new []{"house","loan","food","social life", "car","traveling"});
+        }
+    }
+}
